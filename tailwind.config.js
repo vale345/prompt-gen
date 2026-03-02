db6cd6c46a848a158058ebc4fc491d92157a5983
Valeria <valeushuaia@hotmail.com>
2026-03-01 21:38:49 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', 'system-ui', 'sans-serif'],
-                heading: ['"DM Sans"', 'system-ui', 'sans-serif'],
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                heading: ['DM Sans', ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 brand: {
                     DEFAULT: '#4a6cf7',
                     dark: '#3b5de7',
-                    light: '#e8edfe',
+                    light: '#eef0fe',
                 },
                 accent: {
                     DEFAULT: '#22c997',
-                    dark: '#1ab080',
-                    light: '#e6faf4',
+                    light: '#e6faf5',
+                    dark: '#179e77',
                 },
                 surface: '#ffffff',
                 muted: '#6b7b93',
@@ -27,4 +31,4 @@ export default {
         },
     },
     plugins: [],
-};
+}
