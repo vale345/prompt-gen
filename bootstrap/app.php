cf4facc92b91b4503816378253899a737121a377
vale345 <conecta.valeriam@gmail.com>
2026-03-04 14:03:07 -0300

    Enforce prompt quota for free plan users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'prompt.quota' => \App\Http\Middleware\EnsurePromptQuota::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
